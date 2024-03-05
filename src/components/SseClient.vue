d810c08b5eda4097f2a8fac35c03845b9f818ca3
shenjy24 <shenjy24@gmail.com>
2024-03-05 15:25:04 +0800

    feat:调整SSE

--- src/components/SseClient.vue
+++ src/components/SseClient.vue
@@ -1,32 +1,28 @@
 <script setup>
-import {onMounted, onUnmounted, ref} from 'vue'
-import axios from "axios";
+import {onBeforeUnmount, onMounted, ref} from 'vue'
 
 let message = ref("")
 let lastUpdate = ref("")
 let clientId = ref(1)
-
-function disconnectSse() {
-  axios.get(`http://localhost:8080/sse/over?clientId=${clientId.value}`)
-}
+let eventSource = ref()
 
 onMounted(() => {
-  const eventSource = new EventSource(`http://localhost:8080/sse/subscribe?clientId=${clientId.value}`)
-  eventSource.addEventListener('message', (event) => {
+  eventSource.value = new EventSource(`http://localhost:8080/sse/subscribe?clientId=${clientId.value}`)
+  eventSource.value.addEventListener('message', (event) => {
     const data = JSON.parse(event.data);
     console.log('sse data:', data)
     message.value = data.message;
     lastUpdate.value = data.timestamp;
   })
 
-  eventSource.addEventListener("error", (error) => {
+  eventSource.value.addEventListener("error", (error) => {
     console.error('sse error:', error);
-    disconnectSse()
+    eventSource.value.close();
   })
 })
 
-onUnmounted(() => {
-  disconnectSse()
+onBeforeUnmount(() => {
+  eventSource.value.close() // 卸载
 })
 </script>
 
